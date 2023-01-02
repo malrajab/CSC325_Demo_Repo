85827004bb88066b2b65c7e5ca39111a9120c6be
Moaath Alrajab <moaath@alrajab.net>
2023-01-01 19:11:04 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/csc325_demo01/CSC325_Demo01.java
@@ -0,0 +1,17 @@
+package com.mycompany.csc325_demo01;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class CSC325_Demo01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Moaath Alrajab \n\n\n");
+
+        System.out.println("Moaath Alrajab \n\n\n");
+        System.out.println("Moaath Alrajab \n\n\n");
+        System.out.println("Moaath Alrajab \n\n\n");
+    }
+}
