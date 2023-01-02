f39e63b57c59f6a1d1318b0b856c0fce520530a1
Moaath Alrajab <moaath@alrajab.net>
2023-01-01 19:19:22 -0500

    Worked on the NewClass setters, getters, and constructor

--- src/main/java/com/mycompany/csc325_demo01/NewClass.java
+++ src/main/java/com/mycompany/csc325_demo01/NewClass.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.csc325_demo01;
 
 /**
@@ -9,5 +6,23 @@ package com.mycompany.csc325_demo01;
  * @author MoaathAlrajab
  */
 public class NewClass {
+    
+    private int age;
+
+    public NewClass() {
+    }
+
+    public NewClass(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
     
 }
